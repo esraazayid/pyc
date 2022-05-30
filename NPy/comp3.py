b3087ebbf089e250fc3291ee256b3cc3623ebec0
esraazayid <106156613+esraazayid@users.noreply.github.com>
2022-05-30 14:40:54 +0400

    Add files via upload

---
+++ NPy/comp3.py
@@ -0,0 +1,10 @@
+a = int(input('Enter num a:'))
+b = int(input('Enter num b:'))
+c = int(input('Enter num c:'))
+if a>b and a<c:
+    print('a is bigger')
+elif b>a and b>c:
+    print('b is bigger')
+else:
+    print('c is bigger')
+    
